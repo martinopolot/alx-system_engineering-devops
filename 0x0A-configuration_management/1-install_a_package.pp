b086885dc748c12419b5bbb219507ac0ea85b656
martinopolot <maopolot@outlook.com>
2023-03-24 12:54:17 -0700

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,7 @@
 
 
 exec { 'install python packages':
-    command   => 'pip3 install --force Flask==2.1.0 flask_restful apiai werkzeug==2.1.1',
-    path => ['/usr/bin/'],
-    unless  => '/usr/bin/test -f /usr/local/lib/python3.2/dist-packages/flask/app.py'
+     command   => 'pip3 install --force Flask==2.1.0 flask_restful apiai werkzeug==2.1.1',
+     path => ['/usr/bin/'],
+     unless  => '/usr/bin/test -f /usr/local/lib/python3.2/dist-packages/flask/app.py'
 }
