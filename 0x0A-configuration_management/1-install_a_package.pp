dfe40cbf4ffa26af27fd47a4e3decdb029162c75
martinopolot <maopolot@outlook.com>
2023-03-24 11:59:55 -0700

    install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,8 @@
-# install flask -v 2.1.0 from pip3
+# install flask -v2.1.0 from pip3
 
 
 exec { 'install python packages':
-     command   => 'pip3 install flask flask_restful apiai',
+     command   => 'pip3 install --force Flask==2.1.0 flask_restful apiai',
      path => ['/usr/bin/'],
-     unless  => '/usr/bin/test -f /usr/local/lib/python3.2.10/dist-packages/flask/app.py'
+     unless  => '/usr/bin/test -f /usr/local/lib/python3.2/dist-packages/flask/app.py'
 }
