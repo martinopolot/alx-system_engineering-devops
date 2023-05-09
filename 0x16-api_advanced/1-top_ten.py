1ec9db57b89789c8613b4301a6ccd4eaf0995a6f
martinopolot <maopolot@outlook.com>
2023-05-09 21:39:45 +0000

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+request module imported
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    all_data = response.json()
+
+    try:
+        raw1 = all_data.get('data').get('children')
+
+        for i in raw1:
+            print(i.get('data').get('title'))
+
+    except:
+        print("None")
